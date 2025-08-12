798dee771f5f7f578269549dd949efebcf189873
bolnica041 <bolnica041@gmail.com>
2025-08-12 15:44:29 +0200

    Added finishing touches

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -80,7 +80,7 @@ export default async function Home() {
         <CarouselWithNav images={images} />
       </div>
       <Container className="mt-24 md:mt-28">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
+        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-12 lg:max-w-none lg:grid-cols-2">
           <div className="flex flex-col gap-16">
             <ContactForm />
           </div>
